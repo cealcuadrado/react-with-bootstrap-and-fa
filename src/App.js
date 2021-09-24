3326aab15ed9a532ef0513fec91ef2a94f88b03a
CeAlCuadrado <cealcuadrado@outlook.com>
2021-09-24 19:45:13 -0300

    Instaladas dependencias

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1><i className="fa fa-user fa-fw"></i> User</h1>
+      <hr />
+      <p>Hello, in this app I've imported successfully <strong className="text-danger"><i className="fab fa-bootstrap fa-fw"></i> Bootstrap</strong> and <strong className="text-success"><i className="fab fa-fort-awesome fa-fw"></i>Fort Awesome</strong>.</p>
+
+      <button className="btn btn-success">
+        <i className="fas fa-check fa-fw"></i> Success
+      </button>
     </div>
   );
 }
